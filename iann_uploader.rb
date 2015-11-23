d6cc1a82185eb84bae7c7b280463317d6bbad0a4
Niall Beard <niallbeard@gmail.com>
2015-11-23 14:37:44 +0000

    Added more exception fields and deleted commented stuff

--- iann_uploader.rb
+++ iann_uploader.rb
@@ -25,15 +25,9 @@ docs.each do |event|
     case element.values
       when ['id']
         new_event.external_id = element.text
-      when ['category']
-        new_event.category = element.text
-      when ['title']
-        new_event.title = element.text
-      when ['link']
-        new_event.link = element.text
       when ['keyword'], ['public'],
           ['submission_comment'], ['submission_date'], ['submission_name'],
-          ['submission_organization'], ['_version_']
+          ['submission_organization'], ['_version_'], ['submission_email'], ['image']
         puts "Ignored for element type #{element.values}"
       else
         puts "Set event.#{element.values.first} to #{ element.text}"
@@ -52,50 +46,3 @@ docs.each do |event|
   end
 end
 
-
-=begin
-event = Event.new(external_id='ae10fc5c-ba6a-4be8-85c6-24e5310c96b3',
-                  title='Cambridge Computational Biology Institute Annual Symposium',
-                  subtitle='',
-                  link="http://www.ccbi.cam.ac.uk/Events/Workshops/symp_12.php",
-                  provider='CCBI',
-                  field='Bioinformatics',
-                  description='',
-                  category=["meeting", "event"],
-                  start_date='2012-05-24T23:00:00Z',
-                  end_date='2012-05-24T23:00:00Z',
-                  sponsor='CCBI',
-                  venue='Centre for Mathematical Sciences',
-                  city='Cambridge',
-                  county='',
-                  country='United Kingdom',
-                  postcode='',
-                  latitude=52.2101038,
-                  longitude=0.1019566)
-
-
-=begin
-
-false_material = Material.new(title = 'No such material has yet been uploaded',
-                              url = 'https://www.elixir-europe.org/',
-                              short_description = 'So this should fail',
-                              doi = 'N/A',
-                              remote_updated_date = Time.now,
-                              remote_created_date = nil)
-
-=end
-=begin
-
-check = Uploader.check_event(event)
-#check = Uploader.check_material(false_material)
-puts check.inspect
-
-if check.empty?
-  puts 'No record by this name found. Creating it...'
-  result = Uploader.create_event(event)
-  puts result.inspect
-else
-  puts 'A record by this name already exists.'
-end
-
-=end
