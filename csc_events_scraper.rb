83f4ee40bdd24f4977a9d468ee4ce02472c55c05
Milo Thurston <knirirr@gmail.com>
2016-02-09 10:10:42 +0000

    Cleaned up CSC venue string - some have a typo in.

--- csc_events_scraper.rb
+++ csc_events_scraper.rb
@@ -85,7 +85,7 @@ $events.each_key do |key|
       venue = row.css('td')[1].text.strip
       # Strip out certain long strings we don't need in the venue.
       venue.gsub!(/^The event is organised at the /,"")
-      venue.gsub!(/ The best way to reach us is by public transportation; more detailed travel tips are available.$/,"")
+      venue.gsub!(/ The best way to reach us is by public transportation; more detailed travel tips(\)?) are available.$/,"")
     end
   end
 
