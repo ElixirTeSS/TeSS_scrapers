bd20dc51d18e94b8f5abf877853949cc2670a97d
Niall Beard <niallbeard@gmail.com>
2016-03-05 21:31:58 +0000

    completed TGAC transfer script using the two TeSS APIs

--- tgac_trasfer.rb
+++ tgac_trasfer.rb
@@ -1,3 +1,4 @@
+require 'tess_api'
 require 'open-uri'
 tgac_mat = open 'https://tess.elixir-uk.org/api/3/action/organization_show?id=the-genome-analysis-centre&include_datasets=true', {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}
 tgac_mats = JSON.parse(tgac_mat.read)
@@ -11,4 +12,18 @@ cp = ContentProvider.new(
   provider['image_display_url'], #logo
   provider['description'], #description
 )
-cp.Uploader.create_or_update_content_provider(cp)
+cp = Uploader.create_or_update_content_provider(cp)
+
+provider['packages'].each do |package|
+  material = open "https://tess.elixir-uk.org/api/3/action/package_show?id=#{package['name']}", {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE} 
+  material = JSON.parse(material.read) if material 
+  material = material['result']
+  new_material = Material.new
+  new_material.title = material['title']  
+  new_material.url = material['url']
+  new_material.short_description = material['notes']
+  new_material.keywords = material['tags']
+  new_material.content_provider_id = cp['id']
+  Uploader.create_or_update_material(new_material)
+end
+
