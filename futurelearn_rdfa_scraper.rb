7ea415a1c4763ab61c2df4daa2f5c57dfd79a305
Niall Beard <niallbeard@gmail.com>
2016-05-17 15:29:51 +0100

    follow links to extract decription

--- futurelearn_rdfa_scraper.rb
+++ futurelearn_rdfa_scraper.rb
@@ -21,16 +21,24 @@ cp = Uploader.create_or_update_content_provider(cp)
 
 
 events.each do |event|
-	   begin
-        upload_event = Event.new(id=nil,
-     content_provider_id = cp['id'],
-     external_id = nil,
-     title = event['schema:name'], 
-     subtitle = nil,
-     url = event['schema:url'],
-     provider = event['schema:organizer'],
-     field = nil,
-     description = event['schema:description']
+    rdfa = RDF::Graph.load(event['schema:url'], format: :rdfa)
+    event = RdfaExtractor.parse_rdfa(rdfa, 'Event')
+    begin
+        event = event.first
+        upload_event = Event.new(
+            id=nil,
+            content_provider_id = cp['id'],
+            external_id = nil,
+            title = event['schema:name'], 
+            subtitle = nil,
+            url = event['schema:url'],
+            provider = event['schema:organizer'],
+            field = nil,
+            description = event['schema:description'],
+            keywords = [],
+            category = nil,
+            start_date = event['schema:startDate'],
+            end_date = event['schema:startDate'] # + event['schema:duration']
           ) 
         Uploader.create_or_update_event(upload_event)
         rescue => ex
