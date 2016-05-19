3b3b4763dac008756bd75a2d4c50b5a3e0cf939c
Milo Thurston <knirirr@gmail.com>
2016-05-19 13:58:56 +0100

    Basic Coursera scraper for https://github.com/ElixirUK/TeSS/issues/222

--- coursera_scraper.rb
+++ coursera_scraper.rb
@@ -4,30 +4,68 @@ require 'open-uri'
 require 'json'
 require 'tess_api'
 
+# For more details of the API and available terms, see:
 # https://building.coursera.org/app-platform/catalog/
 
 
 $lessons = {}
 $debug = false
 $root_url = 'https://api.coursera.org/api/courses.v1'
+$friendly_url = 'https://www.coursera.org/learn/'
 $owner_org = 'coursera'
 $categories = {}
+$search_term = 'bioinformatics'
+$search_fields = 'description,domainTypes,primaryLanguages,subtitleLanguages'
 
 def parse_data(page)
   return JSON.parse(open($root_url + page).read)
 end
 
+cp = ContentProvider.new(
+    'Coursera',
+    'http://www.coursera.org',
+    'http://upload.wikimedia.org/wikipedia/commons/e/e5/Coursera_logo.PNG',
+    'Coursera is an education platform that partners with top universities and organizations worldwide, to offer courses online for anyone to take, for free.'
+)
+cp = Uploader.create_or_update_content_provider(cp)
 
-puts "DATA: #{parse_data('/?q=search&query=dna')}"
+course_ids = parse_data("/?q=search&query=#{$search_term}&limit=10")['elements'].collect {|x| x['id']}
+course_url = "?ids=#{course_ids.join(',')}&fields=#{$search_fields}"
+parse_data(course_url)['elements'].each do |course|
+  next unless course['primaryLanguages'].include?('en') # There are some Russian courses turning up...
+  topics = [course['domainTypes'].collect {|x| x['domainId'] }, course['domainTypes'].collect {|x| x['subdomainId'] }].flatten.uniq
+  begin
+    material = Material.new(title = course['name'],
+                            url = $friendly_url + course['slug'],
+                            short_description = course['description'],
+                            doi = nil,
+                            remote_updated_date = Time.now,
+                            remote_created_date = nil,
+                            content_provider_id = cp['id'],
+                            scientific_topic_names = [],
+                            keywords = topics,
+                            licence = nil,
+                            difficulty_level = nil,
+                            contributors = [],
+                            authors = nil,
+                            target_audience = nil
+    )
+    puts material.inspect
+    Uploader.create_or_update_material(material)
+  rescue => ex
+    puts ex.message
+  end
 
-# Create the organisation.
-=begin
-org_title = 'Coursera'
-org_name = $owner_org
-org_desc = 'Coursera is an education platform that partners with top universities and organizations worldwide, to offer courses online for anyone to take, for free.'
-org_image_url = 'http://upload.wikimedia.org/wikipedia/commons/e/e5/Coursera_logo.PNG'
-homepage = 'https://www.coursera.org/'
-node_id = ''
-organisation = Organisation.new(org_title,org_name,org_desc,org_image_url,homepage,node_id)
-Uploader.check_create_organisation(organisation)
-=end
+
+
+end
+
+
+
+
+
+__END__
+{"id"=>"M5IS7Pw3EeW3kAo3Iffzfw",
+"description"=>"In this class, we will compare DNA from an individual against a reference human genome to find potentially disease-causing mutations. We will also learn how to identify the function of a protein even if it has been bombarded by so many mutations compared to similar proteins with known functions that it has become barely recognizable.",
+"domainTypes"=>[{"domainId"=>"life-sciences", "subdomainId"=>"bioinformatics"}, {"subdomainId"=>"algorithms", "domainId"=>"computer-science"}],
+"slug"=>"dna-mutations", "courseType"=>"v2.ondemand", "subtitleLanguages"=>[], "name"=>"Finding Mutations in DNA and Proteins (Bioinformatics VI)", "primaryLanguages"=>["en"]}
