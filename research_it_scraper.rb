f687bb15ba412b6114de6501068ebc1d0bc1129f
Milo Thurston <knirirr@gmail.com>
2016-06-08 17:59:14 +0100

    Research IT scraper still not useful, but it's been updated.

--- research_it_scraper.rb
+++ research_it_scraper.rb
@@ -8,7 +8,7 @@ require 'tess_api_client'
 $root_url = 'https://www.ucl.ac.uk/isd/services/research-it/training'
 $owner_org = 'research-it'
 $lessons = {}
-$debug = ScraperConfig.debug?
+$debug = false #ScraperConfig.debug?
 
 
 # Do the parsing...
@@ -23,29 +23,30 @@ def parse_data(page)
       puts "Failed to open research-it.html file."
     end
   else
-    puts "Opening: #{$root_url + page}"
-    doc = Nokogiri::HTML(open($root_url + page))
+    puts "Opening: #{page}"
+    doc = Nokogiri::HTML(open(page))
   end
 
-  # Doesn't work, of course. Surprise!
-  doc.xpath('//section').each do |record|
-    puts record.inspect
+  doc.xpath('//h3/a').each do |record|
+    url = record['href']
+    name = record.content
+    $lessons[url] = {}
+    $lessons[url]['name'] = name
+    page = Nokogiri::HTML(open(url))
+    desc = page.xpath('//p')[1].content
+    if desc == ''
+      $lessons[url]['short_description'] = 'No description available at this time.'
+    else
+      $lessons[url]['short_description'] = desc
+    end
   end
-
-
 
 end
 
 
 
 # Actually run the code here...
-if $debug
-  parse_data('a random string')
-else
-  parse_data($root_url)
-end
-
-__END__
+parse_data($root_url)
 
 
 # Update the records...
@@ -62,15 +63,15 @@ cp = Uploader.create_or_update_content_provider(cp)
 
 # Create the new record
 $lessons.each_key do |key|
-  material = Material.new(title = $lessons[key]['name'],
-                          url = $root_url + key,
-                          short_description = "#{$lessons[key]['name']} from #{$root_url + key}.",
-                          doi = nil,
-                          remote_updated_date = Time.now,
-                          remote_created_date = $lessons[key]['last_modified'],
-                          content_provider_id = cp['id'],
-                          scientific_topic = [],
-                          keywords = [])
+  material = Material.new({title: $lessons[key]['name'],
+                          url: key,
+                          short_description: $lessons[key]['short_description'],
+                          doi: nil,
+                          remote_updated_date: Time.now,
+                          remote_created_date: nil,
+                          content_provider_id: cp['id'],
+                          scientific_topic: [],
+                          keywords:[]})
 
   Uploader.create_or_update_material(material)
 end
