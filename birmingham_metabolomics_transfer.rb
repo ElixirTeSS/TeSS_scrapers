9fb41a5ff8e3a2db8c950cf5efc20d036fe93373
Aleksandra Nenadic <a.nenadic@manchester.ac.uk>
2016-06-17 12:13:20 +0100

    Updated BMC's logo URL with their new logo

--- birmingham_metabolomics_transfer.rb
+++ birmingham_metabolomics_transfer.rb
@@ -9,7 +9,7 @@ materials = provider['packages']
 cp = ContentProvider.new(
   provider['display_name'], #name
   provider['homepage'], #url
-  'https://pbs.twimg.com/profile_images/620946986070601728/nI-nK5xj_400x400.jpg', #logo
+  'http://preview.birmingham.ac.uk/Images/College-LES-only/biosciences/facilities/bmtc/12106-BMTC-lock-up-AW-02-small.png', #logo
   provider['description'], #description
 )
 cp = Uploader.create_or_update_content_provider(cp)
