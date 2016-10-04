4f5b04212c9049ad9e5e93c78d85fadc8e977878
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-04 10:43:27 +0100

    Moved goblet API scraper back. Changed paths in run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # TRAINING MATERIALS
-ruby goblet_api_scraper.rb
-#ruby goblet_scraper.rb # Old one
-ruby genome3d_scraper.rb
-ruby ebi_scraper.rb
-ruby sib_scraper.rb
-ruby bitsvib_scraper.rb
-ruby legacy_software_carpentry_scraper.rb
+ruby scrapers/goblet_api_scraper.rb
+#ruby scrapers/goblet_scraper.rb # Old one
+ruby scrapers/genome3d_scraper.rb
+ruby scrapers/ebi_scraper.rb
+ruby scrapers/sib_scraper.rb
+ruby scrapers/bitsvib_scraper.rb
+ruby scrapers/legacy_software_carpentry_scraper.rb
 
 
 # EVENTS
-ruby csc_events_scraper.rb
-ruby dtls_events.rb
-ruby elixir_events_scraper.rb
-ruby iann_events_uploader.rb
+ruby scrapers/csc_events_scraper.rb
+ruby scrapers/dtls_events.rb
+ruby scrapers/elixir_events_scraper.rb
+ruby scrapers/iann_events_uploader.rb
 
 #NOT READY
-#ruby bitsvib_event_scraper.rb
+#ruby scrapers/bitsvib_event_scraper.rb
 
