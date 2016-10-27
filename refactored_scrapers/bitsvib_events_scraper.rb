6edff56293c5230e31d0916d07e25b85bdb01558
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-27 15:03:36 +0100

    Refactored BitsvibEventsScraper

---
+++ refactored_scrapers/bitsvib_events_scraper.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+require_relative '../tess_scraper.rb'
+require 'nokogiri'
+
+class BitsvibEventsScraper < TessScraper
+
+  def self.config
+    {
+        name: 'VIB Bioinformatics Training and Services Scraper',
+        root_url: 'http://www.vib.be/en/training/research-training/courses/Pages/default.aspx',
+        categories: %w{Bioinformatics, Skills, Science, Coaching}
+    }
+  end
+
+  def scrape
+    cp = add_content_provider(
+        { title: "VIB Bioinformatics Training and Services",
+          url: "https://www.bits.vib.be/",
+          image_url: "http://www.vib.be/VIBMediaLibrary/Logos/Service_facilities/BITS_website.jpg",
+          description: "Provider of Bioinformatics and software training, plus informatics services and resource management support.",
+          content_provider_type: ContentProvider::PROVIDER_TYPE[:ORGANISATION],
+          node: Node::NODE_NAMES[:BE]
+        })
+
+    config[:categories].each do |cat|
+      url = "#{config[:root_url]}?VIBCourseCategory=#{cat}"
+      puts "Opening: #{url}"
+      doc = Nokogiri::HTML(open(url))
+      process_html(doc)
+    end
+  end
+
+  private
+
+  def process_html(doc)
+    # TODO: Come up with a means to process this which actually works
+    doc.search('table > tr').each do |row|
+      puts "ROW: #{row}"
+    end
+  end
+
+end
