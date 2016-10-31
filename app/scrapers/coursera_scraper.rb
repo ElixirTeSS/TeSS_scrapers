e5904c930d47d3a9a8bb6b00e01ff1eea2ab7134
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-10-31 12:06:20 +0000

    Refactored CourseraScraper

---
+++ app/scrapers/coursera_scraper.rb
@@ -0,0 +1,49 @@
+class CourseraScraper < Tess::Scrapers::Scraper
+  # For more details of the API and available terms, see:
+  # https://building.coursera.org/app-platform/catalog/
+
+  def self.config
+    {
+        name: 'Coursera Scraper',
+        offline_url_mapping: {},
+        root_url: 'https://api.coursera.org/api/courses.v1',
+        friendly_url: 'https://www.coursera.org/learn/',
+        search_term: 'bioinformatics',
+        search_fields: 'description,domainTypes,primaryLanguages,subtitleLanguages'
+    }
+  end
+
+  def scrape
+    cp = add_content_provider(Tess::API::ContentProvider.new(
+        { title: "Coursera",
+          url: "http://www.coursera.org",
+          image_url: "http://logonoid.com/images/coursera-logo.png",
+          description: "Coursera is an education platform that partners with top universities and organizations worldwide, to offer courses online for anyone to take, for free.",
+          content_provider_type: Tess::API::ContentProvider::PROVIDER_TYPE[:PORTAL]
+        }))
+
+    course_ids = parse_data("/?q=search&query=#{config[:search_term]}&limit=10")['elements'].collect {|x| x['id']}
+    course_url = "?ids=#{course_ids.join(',')}&fields=#{config[:search_fields]}"
+    parse_data(course_url)['elements'].each do |course|
+      next unless course['primaryLanguages'].include?('en') # There are some Russian courses turning up...
+      topics = [course['domainTypes'].collect {|x| x['domainId'] }, course['domainTypes'].collect {|x| x['subdomainId'] }].flatten.uniq
+
+      add_material(Tess::API::Material.new(
+          { title: course['name'],
+            url: config[:friendly_url] + course['slug'],
+            short_description: course['description'],
+            remote_updated_date: Time.now,
+            remote_created_date: nil,
+            content_provider: cp,
+            keywords: topics
+          }))
+    end
+  end
+
+  private
+
+  def parse_data(page)
+    JSON.parse(open_url((config[:root_url]+ page)).read)
+  end
+
+end
