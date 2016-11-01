4d9af0234a9c337c913ca9e7378b56b3177fbb8b
Niall Beard <niallbeard@gmail.com>
2016-11-01 11:10:43 +0000

    Added location info

--- scrapers/cambrige_events.rb
+++ scrapers/cambrige_events.rb
@@ -42,10 +42,10 @@ $lessons = {}
 $debug = ScraperConfig.debug?
 
 cp = ContentProvider.new({
-                             title: "University of Cambridge", #name
+                             title: "University of Cambridge Bioinformatics Training", #name
                              url: "http://training.csx.cam.ac.uk/bioinformatics/", #url
                              image_url: "http://www.crim.cam.ac.uk/global/test/images/logo.png", #logo
-                             description: "", #description
+                             description: "We offer a broad range of undergraduate and postgraduate hands-on training courses focused on bioinformatics and computational biology. These training activities aim at enabling life scientists to effectively handle and interpret biological data.", #description
                              content_provider_type: ContentProvider::PROVIDER_TYPE[:ORGANISATION],
                              node: Node::NODE_NAMES[:'UK']
                          })
@@ -61,19 +61,30 @@ programmes = json['result']['programmes']
 #Events are separated into years. There are three programmes: bioinfo-2015, bioinfo-2016, bioinfo-2017. Last one currently doesn't have dates - check for startDate before adding
 programmes.each do |programme|
   programme['events'].last(30).each do |event|
-    if !event['startDate'].nil?
-       event = Event.new({
-          title: event['title'],
-          content_provider_id: cp['id'],
-          url: root_url + event['eventId'].to_s,
-          description: markdownify_urls(event['description']),
-          start_date: Time.at(event['startDate'].to_f/1000), #Remove milliseconds before parsing
-          end_date: Time.at(event['endDate'].to_f/1000),
-          target_audience: parse_audience(event['targetAudience']),
-          event_types: [Event::EVENT_TYPE[:workshops_and_courses]],
-          organizer: "University of Cambridge"
-      })
-      event = Uploader.create_or_update_event(event)
+    if !event['startDate'].nil? and 
+      if event['title'].match(/^[CRUK:]/)
+        puts "Skipping Cancer Research UK event"
+      else
+         event = Event.new({
+            title: event['title'],
+            content_provider_id: cp['id'],
+            url: root_url + event['eventId'].to_s,
+            description: markdownify_urls(event['description']),
+            start_date: Time.at(event['startDate'].to_f/1000), #Remove milliseconds before parsing
+            end_date: Time.at(event['endDate'].to_f/1000),
+            target_audience: parse_audience(event['targetAudience']),
+            event_types: [Event::EVENT_TYPE[:workshops_and_courses]],
+            organizer: "University of Cambridge",
+            host_institutions: ['University of Cambridge Bioinformatics Training'],
+            venue: 'Craik-Marshall Building',
+            city: 'Cambridge',
+            country: 'United Kingdom',
+            postcode: 'CB2 3AR',
+            latitude: 52.2019652,
+            longitude: 0.1224858
+        })
+        event = Uploader.create_or_update_event(event)
+      end
     end
   end
 end
