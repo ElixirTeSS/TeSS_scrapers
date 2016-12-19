f4f875104ed2eb92097a3eabdb47880ab90bd126
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-12-19 14:26:39 +0000

    Use Geocoder to fetch SWC events' city and country. Fixes ElixirUK/TeSS#341

--- app/scrapers/software_carpentry_events_scraper.rb
+++ app/scrapers/software_carpentry_events_scraper.rb
@@ -9,6 +9,7 @@ class SoftwareCarpentryEventsScraper < Tess::Scrapers::Scraper
         offline_url_mapping: {},
         root_url: 'http://software-carpentry.org',
         ical_path: '/workshops.ics',
+        geocoder_cache: {} # A little cache to stop duplicate queries
     }
   end
 
@@ -24,34 +25,49 @@ class SoftwareCarpentryEventsScraper < Tess::Scrapers::Scraper
     file = open_url(config[:root_url] + config[:ical_path])
     events = Icalendar::Event.parse(file.set_encoding('utf-8'))
 
-    events.each do |event|
-      begin
-        #@client = GooglePlaces::Client.new(ScraperConfig.google_api_key)
-        #google_place = @client.spots_by_query(event.location, :language => 'en')
-        #google_place = google_place.first || nil
-        if event.description.start_with?('http://cannot.find.url/') && verbose
-          puts "skipping #{event.summary} - no URL"
-        else
-          summary = [event.summary].flatten.join(' ')
-          description = [event.description].flatten.join(' ')
-          add_event(Tess::API::Event.new(
-              { content_provider: cp,
-                title: "Software Carpentry - #{summary}",
-                url: description,
-                start: event.dtstart,
-                end: event.dtend,
-                description: "Find out more at #{description}",
-                organizer: 'Software Carpentry',
-                event_types: [:workshops_and_courses],
-                latitude: event.geo.first,
-                longitude: event.geo.last
-              }))
+    events.each_slice(40) do |batch|
+      batch.each do |event|
+        begin
+          city = nil
+          country = nil
+          lat = event.geo.first
+          lon = event.geo.last
+          unless geo_cache(lat, lon).nil?
+            city = geo_cache(lat, lon).address_components.select { |c| c['types'].include?('locality') }.first
+            city = city['long_name'] if city
+            country = geo_cache(lat, lon).address_components.select { |c| c['types'].include?('country') }.first
+            country = country['long_name'] if country
+          end
+          if event.description.start_with?('http://cannot.find.url/') && verbose
+            puts "skipping #{event.summary} - no URL"
+          else
+            summary = [event.summary].flatten.join(' ')
+            description = [event.description].flatten.join(' ')
+            add_event(Tess::API::Event.new(
+                { content_provider: cp,
+                  title: "Software Carpentry - #{summary}",
+                  url: description,
+                  start: event.dtstart,
+                  end: event.dtend,
+                  description: "Find out more at #{description}",
+                  organizer: 'Software Carpentry',
+                  event_types: [:workshops_and_courses],
+                  latitude: lat,
+                  longitude: lon,
+                  venue: event.location,
+                  city: city,
+                  country: country
+                }))
+          end
         end
       end
-      #,
-      #    venue: event.location,
-      #    city: google_place.city,
-      #    country: google_place.country
+      sleep 1.5 # Sleep between batches to avoid hitting Google's API rate limit (temporarily until we can find a better way)
     end
+  end
+
+  private
+
+  def geo_cache(lat, lon)
+    config[:geocoder_cache]["#{lat},#{lon}"] ||= Geocoder.search([lat, lon]).first
   end
 end
