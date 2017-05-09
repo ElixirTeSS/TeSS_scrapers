1ac9aa77da14f831084f388a9cb890628ed50d2a
Niall Beard <niallbeard@gmail.com>
2017-05-09 15:16:18 +0100

    Added some custom RDF terms that haven't been accepted to schema.org

--- lib/tess/scrapers/rdf_event_extractor.rb
+++ lib/tess/scrapers/rdf_event_extractor.rb
@@ -21,6 +21,15 @@ module Tess
           if !params[:end] && params[:start] && duration
             params[:end] = modify_date(params[:start], duration)
           end
+          email = params.delete(:contact_email)
+          name = params.delete(:contact_name)
+          if email and name
+            params[:contact] = "#{name} - #{email}"
+          elsif email
+            params[:contact] = email
+          elsif name
+            params[:contact] = name
+          end
 
           Tess::API::Event.new(params)
         end
@@ -30,11 +39,13 @@ module Tess
 
       def self.singleton_attributes
         [:title, :description, :start, :end, :venue, :postcode, :locality, 
-          :organizer, :duration, :url, :country, :latitude, :longitude]
+          :organizer, :duration, :url, :country, :latitude, :longitude, 
+          :contact_name, :contact_email, :contact
+          ]
       end
 
       def self.array_attributes
-        []
+        [:keywords, :scientific_topic_names, :host_institutions]
       end
 
       def self.type_query
@@ -53,6 +64,8 @@ module Tess
               pattern RDF::Query::Pattern.new(event_uri, RDF::Vocab::SCHEMA.organizer, :organizer, optional: true)
               pattern RDF::Query::Pattern.new(event_uri, RDF::Vocab::SCHEMA.duration, :duration, optional: true)
               pattern RDF::Query::Pattern.new(event_uri, RDF::Vocab::SCHEMA.url, :url, optional: true)
+              pattern RDF::Query::Pattern.new(event_uri, RDF::Vocabulary::Term.new('http://schema.org/topic', attributes: {}), :scientific_topic_names, optional: true)
+              pattern RDF::Query::Pattern.new(event_uri, RDF::Vocabulary::Term.new('http://schema.org/topic', attributes: {}), :keywords, optional: true)
             end,
             # Other location info
             RDF::Query.new do
@@ -76,7 +89,19 @@ module Tess
               pattern RDF::Query::Pattern.new(:address, RDF::Vocab::SCHEMA.postalCode, :postcode, optional: true)
               pattern RDF::Query::Pattern.new(:address, RDF::Vocab::SCHEMA.addressCountry, :country, optional: true)
               pattern RDF::Query::Pattern.new(:address, RDF::Vocab::SCHEMA.addressLocality, :locality, optional: true)
+            end,
+            #Host institution
+            RDF::Query.new do
+              pattern RDF::Query::Pattern.new(event_uri, RDF::Vocabulary::Term.new('http://schema.org/hostInstitution', attributes: {}), :host_institution)
+              pattern RDF::Query::Pattern.new(:host_institution, RDF::Vocab::SCHEMA.name, :host_institutions, optional: true)
+            end,
+            #Contact 
+            RDF::Query.new do
+              pattern RDF::Query::Pattern.new(event_uri, RDF::Vocabulary::Term.new('http://schema.org/contact', attributes: {}), :contact_details)
+              pattern RDF::Query::Pattern.new(:contact_details, RDF::Vocab::SCHEMA.email, :contact_email, optional: true)
+              pattern RDF::Query::Pattern.new(:contact_details, RDF::Vocab::SCHEMA.name, :contact_name, optional: true)
             end
+
         ]
       end
     end
