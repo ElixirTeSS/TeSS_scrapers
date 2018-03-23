6433757d38eac37300818dce8b29b0921e65781d
Niall Beard <niallbeard@gmail.com>
2018-03-23 17:44:00 +0000

    Added scraper for sheffield bioinformatics core

---
+++ app/scrapers/sheffield_scraper.rb
@@ -0,0 +1,70 @@
+
+class SheffieldScraper < Tess::Scrapers::Scraper
+
+  def self.config
+    {
+        name: 'Sheffield Bioinformatics Core',
+        offline_url_mapping: {},
+        repo: 'https://github.com/sheffield-bioinformatics-core/sheffield-bioinformatics-core.github.io',
+        gh_events_path: 'training',
+        site_base: 'http://sbc.shef.ac.uk/training',
+        git_repo: '/bc.git'
+    }
+  end
+
+  def scrape
+    cp = add_content_provider(Tess::API::ContentProvider.new(
+        { title: "Sheffield Bioinformatics Core",
+          url: "http://sbc.shef.ac.uk//training/",
+          image_url: "http://sheffield-bioinformatics-core.github.io//images/site-logo.png",
+          description: "",
+          content_provider_type: :organisation
+        }))
+
+    git_setup
+    
+    cache_file_path('git')
+    files = Dir["#{cache_file_path('git')}/#{config[:gh_events_path]}/*.md"]
+    files.reject!{|x|x.include?('index.md')}
+    files.each do |file|
+        eventyaml = YAML.load_file(file)
+        event = Tess::API::Event.new({
+          content_provider: cp,
+          event_types: [:workshops_and_courses],
+          url: "#{config[:site_base]}/#{File.basename(file).chomp('.md')}",
+          title: eventyaml['title'],
+          venue: eventyaml['venue'],
+          start: formatted_time(eventyaml['startTime'], eventyaml['date']),
+          end: formatted_time(eventyaml['endTime'], eventyaml['date']),
+          country: eventyaml['country']
+         })
+         add_event(event)
+    end
+  end
+
+  private
+
+  #Horrible Hacks. Date formatted as time: 18.3, date: 2018-04-20. 
+  #Need to add a 0 to time string and replace period for colon to get Time.parse to work
+  def formatted_time time,date 
+    if time and date
+      hours = (time.to_s + '0').gsub('.',':')
+      Time.parse("#{date} #{hours}")
+    else
+      return nil
+    end
+  end
+
+  def git_setup
+    git_path = cache_file_path('git', true)
+    unless File.exists?(git_path)
+      puts "Cloning git repo..."
+      %x{git clone #{config[:repo]}.git #{git_path}}
+    end
+    %x{cd #{git_path} && git pull} unless offline
+  end
+
+  def parse_data
+  end
+
+end
