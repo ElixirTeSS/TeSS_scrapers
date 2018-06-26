6407614f7c74b55f933219e9bf6636bd55da84b8
Niall Beard <niallbeard@gmail.com>
2018-06-26 16:24:50 +0100

    New scraper for open targets

---
+++ app/scrapers/open_target_json_scraper.rb
@@ -0,0 +1,72 @@
+class OpenTargetJsonScraper < Tess::Scrapers::Scraper
+
+  def self.config
+    {
+        name: 'Open Targets Scraper',
+        offline_url_mapping: {},
+        json_feed: 'https://raw.githubusercontent.com/opentargets/live-files/gh-pages/outreach.json'
+    }
+  end
+
+  def scrape
+    cp = add_content_provider(Tess::API::ContentProvider.new(
+        { title: "Open Target",
+          url: "https://www.opentargets.org/",
+          #image_url: "",
+          description: "Open Targets is an innovative, large-scale, multi-year, 
+          public-private partnership that uses human genetics and genomics data for
+           systematic drug target identification and prioritisation.",
+          content_provider_type: :organization
+        }))
+
+
+        json = JSON.parse(open(config[:json_feed]).read)
+
+        json = JSON.parse(open('https://raw.githubusercontent.com/opentargets/live-files/gh-pages/outreach.json').read)
+        json['sessions'].each do |session|
+          startDateString = session['date']
+          startDate = Date.parse(session['date'])
+          if startDate > Date.today and session['external'] and
+           session['external']['link'] and !session['external']['link'].empty?
+            puts "Extracting! #{session}\n\n\n"
+            event = Tess::API::Event.new({
+              title: session['description'],
+              description: session['external']['text'],
+              url: session['external']['link'],
+              start: startDateString,
+              end: "#{modify_date(startDateString, session['duration']) if session['duration']}",
+              venue: session['place'].split(',').first,
+              country: session['place'].split(',').last,
+              keywords: ['Open Targets'],
+              content_provider: cp
+            })
+            add_event(event)
+          end
+        end
+  end
+
+  private
+
+    def modify_date(date, duration)
+      if date.is_a?(String)
+        date = Date.parse(date)
+      end
+      matches = duration.match(/P([^T]+)T?(.*)/)
+      date_period = matches[1]
+
+      date_period.scan(/(\d+)([YMWD])/).each do |match|
+        value = match[0].to_i
+        case match[1]
+          when 'Y'
+            date = date >> (12 * value)
+          when 'M'
+            date = date >> value
+          when 'W'
+            date = date + (7 * value)
+          when 'D'
+            date = date + value
+        end
+      end
+      date
+    end
+end
