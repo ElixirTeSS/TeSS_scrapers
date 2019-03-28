1469440adc45f46ac4cf147c36ff036cc2f8b203
Niall Beard <niallbeard@gmail.com>
2019-03-28 14:07:35 +0000

    Removed broken logo

--- app/scrapers/bitsvib_rdfa_scraper.rb
+++ app/scrapers/bitsvib_rdfa_scraper.rb
@@ -15,7 +15,6 @@ class BitsvibRdfaScraper < Tess::Scrapers::Scraper
     cp = add_content_provider(Tess::API::ContentProvider.new(
         { title: "VIB Bioinformatics Training and Services",
           url: "https://www.bits.vib.be/",
-          image_url: "http://www.vib.be/VIBMediaLibrary/Logos/Service_facilities/BITS_website.jpg",
           description: "Provider of Bioinformatics and software training, plus informatics services and resource management support.",
           content_provider_type: :organisation,
           node_name: :BE
