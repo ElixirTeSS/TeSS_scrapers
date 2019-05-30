1c5827e27248b9bc03eb5f8a75da172b8629b0d4
Niall Beard <niallbeard@gmail.com>
2019-05-30 14:10:26 +0100

    Updated image URL and feed link

--- app/scrapers/bitsvib_events_jsonld_scraper.rb
+++ app/scrapers/bitsvib_events_jsonld_scraper.rb
@@ -5,7 +5,7 @@ class BitsvibEventsJsonldScraper < Tess::Scrapers::Scraper
   def self.config
     {
         name: 'VIB Bioinformatics Training and Services Events Scraper',
-        root_url: 'https://dev.bits.vib.be/eulife/all_events.json'
+        root_url: 'https://dev.bits.vib.be/eulife/all_events.php'
     }
   end
 
@@ -13,9 +13,9 @@ class BitsvibEventsJsonldScraper < Tess::Scrapers::Scraper
     cp = add_content_provider(Tess::API::ContentProvider.new(
         { title: "VIB Bioinformatics Training and Services",
           url: "https://www.bits.vib.be/",
-          image_url: "http://www.vib.be/VIBMediaLibrary/Logos/Service_facilities/BITS_website.jpg",
+          image_url: "http://www.vib.be/Style%20Library/VIB%20Styles/Images/Logo.gif",
           description: "Provider of Bioinformatics and software training, plus informatics services and resource management support.",
-          content_provider_type: :organisation,
+          content_provider_type: :organization,
           node_name: :BE
         }))
 
