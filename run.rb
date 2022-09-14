018ef862f5cec087c0d1efd7e4ddf1d40001aa46
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-09-14 15:26:10 +0100

    Don't use `constantize` (from ActiveSupport?)

--- run.rb
+++ run.rb
@@ -16,7 +16,7 @@ else
     # Open log file
     log_file = File.open(log, 'w')
     log_file.puts "Running #{scraper_class}"
-    scraper_class.constantize.new(options).run
+    Object.const_get(scraper_class).new(options).run
     log_file.puts 'Done'
   ensure
     log_file.close
