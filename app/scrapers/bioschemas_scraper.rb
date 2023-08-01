8f9e20ff3ed5bdb3173ae5c7d130bdfb6d29b52f
Finn <finn.bacall@manchester.ac.uk>
2023-08-01 12:37:41 +0100

    Don't error if no bioschemas providers

--- app/scrapers/bioschemas_scraper.rb
+++ app/scrapers/bioschemas_scraper.rb
@@ -16,7 +16,7 @@ class BioschemasScraper < Tess::Scrapers::Scraper
 
     {
       name: 'Bioschemas Scraper',
-      providers: YAML.load(File.read(config_path))['providers'],
+      providers: YAML.load(File.read(config_path))['providers'] || [],
       user_agent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0'
     }
   end
